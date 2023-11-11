99b07c89c9ce0de9ae37ae2ef0ace75523a1a17c
Mikail <miklsuhanov@gmail.com>
2023-11-11 15:47:40 +1000

    add src/Connection.php

---
+++ src/Connection.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Task\Vladlink;
+
+final class Connection
+{
+    public static function connect(): \PDO
+    {
+        if (isset($_ENV['DATABASE_URL'])) {
+            /** @var  array{user: string, pass: string, host: string, port: string, path: string} $databaseUrl */
+            $databaseUrl = parse_url($_ENV['DATABASE_URL']);
+        } else {
+            /** @var  array{user: string, pass: string, host: string, port: string, path: string} $databaseUrl */
+            $databaseUrl = parse_ini_file('../config/database.ini');
+        }
+        $username = $databaseUrl['user'];//
+        $password = $databaseUrl['pass'];
+        $host = $databaseUrl['host'];
+        $dbname = ltrim($databaseUrl['path'], '/');//
+
+        $conStr = "pgsql:host=$host;dbname=$dbname";
+        $pdo = new \PDO($conStr, $username, $password);
+        return $pdo;
+    }
+}
