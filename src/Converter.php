fef985ea44037342b956f055639a1741f276b2bc
Mikail <miklsuhanov@gmail.com>
2023-11-11 15:48:59 +1000

    add src/Converter.php

---
+++ src/Converter.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Task\Vladlink;
+
+class Converter
+{
+    private $data;
+
+    public function __construct($filePath)
+    {
+        $this->data = $this->parseData($filePath);
+        print_r($this->data);
+    }
+
+    private function parseData($file)
+    {
+        $filePath = realpath($file);
+
+        if ($filePath === false) {
+            throw new \Exception("File not found");
+        }
+        $data = file_get_contents($filePath);
+        return json_decode($data, true);
+    }
+
+
+}
