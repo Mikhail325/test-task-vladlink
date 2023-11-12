3c934dc6fedcfb5733bbb67d5c052c4cee1f0c8b
Mikail <miklsuhanov@gmail.com>
2023-11-12 18:31:47 +1000

    add src/BuildTree.php

---
+++ src/BuildTree.php
@@ -0,0 +1,52 @@
+<?php
+
+namespace Task\Vladlink;
+
+use Task\Vladlink\db\RepositoryDB;
+
+class BuildTree
+{
+    private $db;
+    private $tab = "  ";
+    private $neadUrl;
+    private $enclosure;
+
+    public function __construct($neadUrl, $enclosure)
+    {
+        $this->neadUrl = $neadUrl;
+        $this->enclosure = $enclosure;
+        $this->db = new RepositoryDB();
+    }
+
+    public function buildTree()
+    {
+        $result = '';
+        $categories = $this->db->getCategories();
+        foreach ($categories as $categore) {
+            $name = $categore->{'name'};
+            $data = $this->buildUrl($categore);
+            $enclosure = substr_count($data, '/');
+
+            if ($this->enclosure >= --$enclosure || $this->enclosure === null) {
+                $url = $this->neadUrl ? " $data" : "\n";
+                $data = str_repeat($this->tab, $enclosure) . $name . $url ;
+                $result .= $data;
+            } 
+        }
+        return $result;
+    }
+
+    public function buildUrl($categore, $url = '')
+    {
+        $idChild = $categore->{'id'};
+        $idParent = $this->db->getParentId($idChild);
+        $url = '/' . $categore->{'alias'} . $url;
+       
+        if ($idParent) {
+            $categore = $this->db->getCategore($idParent);
+            return $this->buildUrl($categore, $url);
+        }
+
+        return $url . "\n";
+    }
+}
